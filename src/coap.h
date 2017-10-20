24c60a22a289014143b3d2d8df2fa2151d748bcc
Malachi Burke <malachi.burke@gmail.com>
2017-10-20 14:42:59 -0700

    Boilerplate checkin

---
+++ src/coap.h
@@ -0,0 +1,17 @@
+//
+// Created by malachi on 10/20/17.
+//
+
+#ifndef SRC_COAP_H
+#define SRC_COAP_H
+
+namespace moducom { namespace coap {
+
+class CoAP {
+
+};
+
+}}
+
+
+#endif //SRC_COAP_H
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "coap.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
