136405bd9a53580eda6e3e6aa4dd1ebcfc2d5c47
Malachi Burke <malachi.burke@gmail.com>
2017-10-21 08:12:55 -0700

    Switching out string-length-field mode

--- src/string.h
+++ src/string.h
@@ -9,6 +9,8 @@
 #include <string.h>
 #include <stdint.h>
 
+
+
 namespace moducom { namespace std {
 
 // replacement for std::string class which instead uses handle-based memory
@@ -16,20 +18,39 @@ class string {
     typedef coap::Memory memory_t;
 
     memory_t::SmartHandle handle;
+#ifdef FEATURE_STRING_SIZE_FIELD
     // TODO: optimize size out - perhaps smarthandle itself can track it?
     size_t size;
+#endif
+
+private:
+    inline char* lock()
+    {
+        return handle.lock<char>();
+    }
+    inline void unlock() { handle.unlock(); }
 
     // TODO: make this smart enough to not allocate memory until someone actually
     // changes the string
     // TODO: Make final decision about +1 for trailing null or not, or make it a
     // traits kinda thing
 protected:
-    string(const memory_t::SmartHandle& handle, size_t size) :
-            handle(handle),
-            size(size + 1) {}
+    string(const memory_t::SmartHandle& handle
+#ifdef FEATURE_STRING_SIZE_FIELD
+            , size_t size
+#endif
+            ) :
+            handle(handle)
+#ifdef FEATURE_STRING_SIZE_FIELD
+            ,
+            size(size + 1)
+#endif
+            {}
 public:
     string(const char* src, memory_t& pool = memory_t::default_pool) :
+#ifdef FEATURE_STRING_SIZE_FIELD
             size(strlen(src)),
+#endif
             // FIX: somehow size isn't assigned and have to use strlen(src) again
             handle(pool.allocate(src, strlen(src) + 1), pool)
     {
@@ -37,8 +58,14 @@ public:
 
     // TODO: Do also && move operation for C++11
     string(const string& copy_from) :
-            handle(copy_from.handle, copy_from.size),
-            size(copy_from.size)
+#ifdef FEATURE_STRING_SIZE_FIELD
+            size(copy_from.size),
+#endif
+            handle(copy_from.handle
+#ifdef FEATURE_STRING_SIZE_FIELD
+                    , copy_from.size
+#endif
+            )
     {
 
     }
@@ -50,8 +77,12 @@ public:
     {
         char* input = handle.lock<char>();
 
+#ifdef FEATURE_STRING_SIZE_FIELD
         memcpy(output, input, size);
         output[size] = 0;
+#else
+        strcpy(output, input);
+#endif
 
         handle.unlock();
     }
@@ -59,20 +90,37 @@ public:
     string& operator += (const char* src)
     {
         size_t src_len = strlen(src);
+#ifndef FEATURE_STRING_SIZE_FIELD
+        // TODO: optimize this somehow
+        char* dest = lock();
+        size_t size = strlen(dest);
+        unlock();
+        handle.resize(size + src_len + 1);
+        dest = lock();
+        strcat(dest, src);
+#else
         handle.resize(size + src_len + 1);
         char* dest = handle.lock<char>();
         memcpy(dest + size, src, src_len);
         size += src_len;
-        handle.unlock();
+#endif
+        unlock();
         return *this;
     }
 
-    inline string operator +(const char *src) const
+    inline string operator +(const char *src)
     {
         size_t src_len = strlen(src);
+#ifndef FEATURE_STRING_SIZE_FIELD
+        size_t size = strlen(lock());
+#endif
         memory_t::SmartHandle new_handle =
                 handle.append_into_new_experimental(src, size, src_len);
-        return string (new_handle, size + src_len);
+        return string (new_handle
+#ifdef FEATURE_STRING_SIZE_FIELD
+                , size + src_len
+#endif
+        );
     }
 
 
@@ -112,18 +160,12 @@ public:
         auto_ptr_t str(handle);
         char* _str = str;
 
+#ifdef FEATURE_STRING_SIZE_FIELD
         _str[size - 1] = 0;
+#endif
 
         return strcmp(_str, src) == 0;
     }
-
-
-    /*
-    inline char* lock()
-    {
-        return handle.lock<char>();
-    }
-    inline void unlock() { handle.unlock(); } */
 };
 
 }}
