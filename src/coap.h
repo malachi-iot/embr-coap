68f17d8106965316fff39c483171450028020dff
Malachi Burke <malachi.burke@gmail.com>
2017-10-23 17:05:51 -0700

    Adding some Option processing code now
    Thinking we may want to do that more streaming-style

--- src/coap.h
+++ src/coap.h
@@ -11,10 +11,10 @@
 namespace moducom {
 namespace coap {
 
-#define COAP_HEADER_VER_POS     29
-#define COAP_HEADER_TYPE_POS    27
-#define COAP_HEADER_TKL_POS     23
-#define COAP_HEADER_CODE_POS    15
+#define COAP_HEADER_VER_POS     30
+#define COAP_HEADER_TYPE_POS    28
+#define COAP_HEADER_TKL_POS     24
+#define COAP_HEADER_CODE_POS    16
 #define COAP_HEADER_MID_POS     0
 
 #define COAP_HEADER_VER_MASK    (3 << COAP_HEADER_VER_POS)
@@ -22,6 +22,13 @@ namespace coap {
 #define COAP_HEADER_TKL_MASK    (15 << COAP_HEADER_TKL_POS)
 #define COAP_HEADER_CODE_MASK   (255 << COAP_HEADER_CODE_POS)
 #define COAP_HEADER_MID_MASK    (0xFFFF)
+
+
+#define COAP_OPTION_DELTA_POS   4
+#define COAP_OPTION_DELTA_MASK  15
+#define COAP_OPTION_LENGTH_POS  0
+#define COAP_OPTION_LENGTH_MASK 15
+
 
 // See RFC 7252: https://tools.ietf.org/html/rfc7252
 class CoAP
@@ -79,12 +86,17 @@ public:
         uint32_t raw;
 
         template <uint8_t pos, uint32_t mask>
-        inline void mask_setter(uint16_t value)
+        inline void mask(uint16_t value)
         {
             raw = (raw & ~mask) | (value << pos);
-            //raw &= ~mask;
-            //raw |= value << pos;
         }
+
+        template <uint8_t pos, uint32_t mask>
+        inline uint16_t mask() const
+        {
+            return ((raw & mask) >> pos);
+        }
+
 
     public:
         enum TypeEnum
@@ -97,26 +109,106 @@ public:
 
         TypeEnum type() const 
         {
-            return (TypeEnum) ((raw & COAP_HEADER_TYPE_MASK) >> COAP_HEADER_TYPE_POS);
+            return (TypeEnum) mask<COAP_HEADER_TYPE_POS, COAP_HEADER_TYPE_MASK>();
         }
 
         void type(TypeEnum type)
         {
-            /*
-            raw &= ~COAP_HEADER_TYPE_MASK;
-            raw |= (int)type << COAP_HEADER_TYPE_POS;
-            */
-            mask_setter<COAP_HEADER_TYPE_POS, COAP_HEADER_TYPE_MASK>((uint16_t)type);
+            mask<COAP_HEADER_TYPE_POS, COAP_HEADER_TYPE_MASK>((uint16_t)type);
         }
+
+        uint8_t code() const
+        {
+            return (uint8_t)mask<COAP_HEADER_CODE_POS, COAP_HEADER_CODE_MASK>();
+        }
+
+
+        void code(uint8_t code)
+        {
+            mask<COAP_HEADER_CODE_POS, COAP_HEADER_CODE_MASK>(code);
+        }
+
+
+        void message_id(uint16_t mid)
+        {
+            mask<COAP_HEADER_MID_POS, COAP_HEADER_MID_MASK>(mid);
+        }
+
+        uint16_t messaged_id() const
+        {
+            return mask<COAP_HEADER_MID_POS, COAP_HEADER_MID_MASK>();
+        }
+
+        void token_length(uint8_t tkl)
+        {
+            ASSERT(true, tkl <= 8);
+
+            mask<COAP_HEADER_TKL_POS, COAP_HEADER_TKL_MASK>(tkl);
+        }
+
+
+        uint8_t token_length()
+        {
+            return (uint8_t) mask<COAP_HEADER_TKL_POS, COAP_HEADER_TKL_MASK>();
+        }
+
 
         Header(TypeEnum type)
         {
             raw = 0;
-            raw |= 1 << (COAP_HEADER_VER_POS + 1);
+            raw |= 1 << COAP_HEADER_VER_POS;
             this->type(type);
         }
     };
 #endif
+
+    class Option
+    {
+        uint8_t bytes[];
+
+        enum ExtendedMode : uint8_t
+        {
+            Extended8Bit = 13,
+            Extended16Bit = 14,
+            Reserved = 15
+        };
+
+        static uint16_t get_value(uint8_t nonextended, uint8_t* extended, uint8_t* index_bump)
+        {
+            if (nonextended < Extended8Bit)
+            {
+                // Just use literal value, not extended
+                return nonextended;
+            }
+            else if (nonextended == Extended8Bit)
+            {
+                (*index_bump)++;
+                return 13 + *extended;
+            }
+            else if (nonextended == Extended16Bit)
+            {
+                // FIX: BEWARE of endian issue!!
+                uint16_t _extended = *((uint16_t*)extended);
+
+                (*index_bump)+=2;
+
+                return 269 + _extended;
+            }
+            else // RESERVED
+            {
+                ASSERT_ERROR(Reserved, nonextended, "Code broken - incorrectly assess value as RESERVED");
+                ASSERT_ERROR(0, 15, "Invalid length/delta value");
+                // TODO: register as an error
+            }
+
+            return 0;
+        }
+
+    public:
+
+
+
+    };
 };
 
 }
