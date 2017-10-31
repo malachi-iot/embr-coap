534768c24ef95595ed24663e5eced49232d6dec8
Malachi Burke <malachi.burke@gmail.com>
2017-10-30 20:21:51 -0700

    Adding SimpleBufferedPipeline, CopiedStatus and experimental by-ref message code

--- src/coap_daemon.h
+++ src/coap_daemon.h
@@ -20,6 +20,7 @@ public:
 
 class PipelineMessage : public MemoryChunk
 {
+    // One might think of Status vs CopiedStatus as WriteStatus vs ReadStatus
 public:
     // Expected this will live on a stack or instance field somewhere
     // With this we can retrieve how our message is progressing through
@@ -36,6 +37,8 @@ public:
         // 6 bits of things just for me!
         // TODO: Consider a flag for memory handle
         // TODO: Consider also a cheap-n-nasty RTTI for extended Statuses
+        // TODO: Consider a demarcation flag to denote a packet boundary.  This gets a little tricky since
+        //       some scenarios have many different boundaries to consider
         uint8_t reserved: 6;
     public:
         bool delivered() const { return _delivered; }
@@ -44,17 +47,51 @@ public:
         bool copied() const {return _copied;}
         void copied(bool value) { _copied = value; }
 
+        // different from regular copy in that only the message itself has been copied
+        // used primarily for by-reference messages vs by-value messages.  Not sure if
+        // a) we're gonna even use by-ref messages and b) if we do whether this will be
+        // useful
+        bool experimental_shallow_copied() const { return false; }
+
         // 8 bits of things just for you!
         uint8_t user : 8;
 
         // Consider an extended version of this with a semaphore
     };
 
+    // this is like regular Status except it's merely copied along with each message,
+    // thus partially alleviating memory management issue with regular Status*
+    // naturally, because of this, copied_status can't be used to report status back to
+    // the originator
+    struct CopiedStatus
+    {
+        // // 8 bits of things just for you!
+        // note in buffered situations, only the last user status is carried.  This contradicts
+        // boundary behavior described below, so plan accordingly when using boundaries
+        uint32_t user : 8;
+
+        // boundaries tend to be application specific, so use this for boundary types 1-7, 0 being
+        // no boundary.  Only restriction is that in buffered situations, larger boundary types will "eat"
+        // smaller ones until buffer is read back out again:
+        //
+        // ...1...1...2...3...1...2...1
+        //                ^       ^
+        // buffer read ---+     --+
+        //
+        // becomes
+        // ...............3.......2...
+        uint32_t boundary : 3;
+    } copied_status;
+
     Status* status;
 
     PipelineMessage() : status(NULLPTR) {}
     PipelineMessage(uint8_t* data, size_t length) : status(NULLPTR),
-                                                    MemoryChunk(data, length) {}
+                                                    MemoryChunk(data, length)
+    {
+        copied_status.user = 0;
+        copied_status.boundary = 0;
+    }
 
 };
 
@@ -136,6 +173,7 @@ public:
 
     virtual bool write(const PipelineMessage& chunk) OVERRIDE
     {
+        // can't write to chunk because spot is filled
         if(this->chunk.length) return false;
 
         this->chunk = chunk;
@@ -143,6 +181,127 @@ public:
         return true;
     }
 };
+
+
+namespace experimental {
+
+// PipelineMessage is starting to get big, experiental code here to handle
+// copying by reference instead of value
+
+class IReferencedReadPipeline
+{
+    virtual const PipelineMessage& read() = 0;
+};
+
+class QueuedReferencePipeline : public IReferencedReadPipeline
+{
+protected:
+    PipelineMessage queue[10];
+
+public:
+    virtual const PipelineMessage& read() OVERRIDE
+    {
+        return queue[0];
+    }
+
+    bool write(const PipelineMessage& message)
+    {
+        queue[0] = message;
+    }
+};
+
+class ConverterPipeline : public QueuedReferencePipeline
+{
+    IPipeline& pipeline;
+
+public:
+    ConverterPipeline(IPipeline& pipeline) : pipeline(pipeline) {}
+
+    virtual const PipelineMessage& read() OVERRIDE
+    {
+        return queue[0] = pipeline.read();
+    }
+
+    bool write(const PipelineMessage& message)
+    {
+        write(message);
+    }
+};
+
+}
+
+
+namespace layer3
+{
+
+// No fancy queue-like behavior, just fills a buffer and expects buffer to be
+// emptied completely out before new space is available
+class SimpleBufferedPipeline : public IPipeline
+{
+    MemoryChunk buffer;
+    size_t length_used;
+
+    // Be careful though, because holding a status that we send out
+    // with the read operation may get overwritten by an incoming
+    // write operation, so we need some kind of indicator that the
+    // one doing the reading is done with the status
+    // Seems sensible to rely on the delivered flag for this, but
+    // then we can easily get a poison-message-ish situation where
+    // the last consumer does not set the delivered flag
+    PipelineMessage::Status status;
+    PipelineMessage::CopiedStatus copied_status;
+
+public:
+    SimpleBufferedPipeline(const MemoryChunk& buffer) :
+            buffer(buffer),
+            length_used(0)
+    {
+        copied_status.boundary = 0;
+    }
+
+    virtual PipelineMessage read() OVERRIDE
+    {
+        PipelineMessage msg;
+
+        if(length_used == 0)
+            msg.data = NULLPTR;
+        else
+            msg.data = buffer.data;
+
+        msg.length = length_used;
+        msg.status = NULLPTR;
+        msg.copied_status = copied_status;
+
+        length_used = 0;
+
+        return msg;
+    }
+
+    virtual bool write(const PipelineMessage& chunk) OVERRIDE
+    {
+        if(chunk.length < (buffer.length - length_used))
+        {
+            if(chunk.copied_status.boundary > copied_status.boundary)
+                copied_status.boundary = chunk.copied_status.boundary;
+
+            copied_status.user = chunk.copied_status.user;
+
+            memcpy(buffer.data + length_used, chunk.data, chunk.length);
+
+            length_used += chunk.length;
+
+            if(chunk.status)
+                chunk.status->copied(true);
+
+            return true;
+        }
+
+        return false;
+    }
+
+};
+
+}
 
 // NOTE: likely would be much better to use the util.embedded streams,
 // but they are so arranged to be low-level it would take a lot of adaptation
