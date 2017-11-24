b5c804844055e3968f0efd7e2b1b17b8323f0d60
Malachi Burke <malachi.burke@gmail.com>
2017-11-24 08:32:32 -0800

    Prepping for move from OptionGenerator::StateMachine to OptionEncoder

---
+++ src/coap-encoder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Malachi Burke on 11/24/17.
+//
+
+#include "coap-encoder.h"
---
+++ src/coap-encoder.h
@@ -0,0 +1,16 @@
+//
+// Created by Malachi Burke on 11/24/17.
+//
+
+#ifndef MC_COAP_TEST_COAP_ENCODER_H
+#define MC_COAP_TEST_COAP_ENCODER_H
+
+#include "coap_transmission.h"
+
+namespace moducom { namespace coap {
+
+typedef experimental::layer2::OptionGenerator::StateMachine OptionEncoder;
+
+}}
+
+#endif //MC_COAP_TEST_COAP_ENCODER_H
