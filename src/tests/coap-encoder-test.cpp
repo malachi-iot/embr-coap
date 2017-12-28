7e0585b06e8e56cb4d68d25f754cbb31684e4bf2
Malachi Burke <malachi.burke@gmail.com>
2017-12-28 11:05:38 -0800

    Prepping non-blocking code

--- src/tests/coap-encoder-test.cpp
+++ src/tests/coap-encoder-test.cpp
@@ -8,19 +8,34 @@ using namespace moducom::pipeline;
 
 typedef CoAP::OptionExperimental::Numbers number_t;
 
-class ExperimentalPrototypeBlockingOptionEncoder1
+class ExperimentalPrototypeOptionEncoder1
 {
+protected:
     typedef moducom::coap::experimental::layer2::OptionGenerator::StateMachine generator_t;
     generator_t generator;
 
-    // option helper
-    uint8_t option(uint8_t* output, number_t number, uint16_t length);
+    // option helper, fills output with non-value portion of option
+    uint8_t _option(uint8_t* output, number_t number, uint16_t length);
 
+    ExperimentalPrototypeOptionEncoder1() : generator(0) {}
+};
+
+
+class ExperimentalPrototypeNonBlockingOptionEncoder1 : public ExperimentalPrototypeOptionEncoder1
+{
+    uint8_t buffer[8];
+
+protected:
+    bool option(IBufferedPipelineWriter& writer, number_t number, const MemoryChunk& value);
+};
+
+
+class ExperimentalPrototypeBlockingOptionEncoder1 : public ExperimentalPrototypeOptionEncoder1
+{
     // option helper
     void option(IPipelineWriter& writer, number_t number, uint16_t length);
 
 public:
-    ExperimentalPrototypeBlockingOptionEncoder1() : generator(0) {}
 
     void option(IPipelineWriter& writer, number_t number, const MemoryChunk& value);
     void option(IPipelineWriter& writer, number_t number)
@@ -63,7 +78,7 @@ public:
 };
 
 
-uint8_t ExperimentalPrototypeBlockingOptionEncoder1::option(uint8_t* output_data, number_t number, uint16_t length)
+uint8_t ExperimentalPrototypeOptionEncoder1::_option(uint8_t* output_data, number_t number, uint16_t length)
 {
     moducom::coap::experimental::layer2::OptionBase optionBase(number);
 
@@ -92,7 +107,8 @@ uint8_t ExperimentalPrototypeBlockingOptionEncoder1::option(uint8_t* output_data
 void ExperimentalPrototypeBlockingOptionEncoder1::option(IPipelineWriter& writer, number_t number, uint16_t length)
 {
     uint8_t buffer[8];
-    uint8_t pos = option(buffer, number, length);
+    // NOTE: Needs underscore on _option as overloading logic can't resolve uint8* vs IPipelineWriter& for some reason
+    uint8_t pos = _option(buffer, number, length);
 
     writer.write(buffer, pos); // generated header-ish portions of option, sans value
 }
@@ -102,6 +118,27 @@ void ExperimentalPrototypeBlockingOptionEncoder1::option(IPipelineWriter& writer
 {
     option(writer, number, value.length);
     writer.write(value); // value portion
+}
+
+
+bool ExperimentalPrototypeNonBlockingOptionEncoder1::option(IBufferedPipelineWriter& writer, number_t number,
+                                                            const MemoryChunk& value)
+{
+    // Need mini-state machine to determine if we are writing value chunk or
+    // pre-value chunk
+    if(generator.state() == CoAP::Parser::OptionSize)
+    {
+        _option(buffer, number, value.length);
+    }
+
+    // TODO: examine writable to acquire non-blocking count.  Not to be confused with the very similar buffered stuff
+    // writable() doesn't exist yet, but should be a part of the base IPipelineWriter - which is looking more and more
+    // like a stream all the time
+    // NOTE: Probably should draw a distinction between Reader/Writer and PipelineReader/PipelineWriter.  remember,
+    // original purpose of pipeline writes were to carry the memory descriptors themselves around.  It seems I've strayed
+    // from that due to all the pieces floating around here.  Maybe we need it fully broken out:
+    // Reader, BufferedReader, PipelineReader, BufferedPipelineReader
+    //writer.writable();
 }
 
 TEST_CASE("CoAP encoder tests", "[coap-encoder]")
