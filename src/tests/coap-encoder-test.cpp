ff921143482df969d7f40495dceb5fadb135b2d9
Malachi Burke <malachi.burke@gmail.com>
2017-12-28 08:19:37 -0800

    Misc

---
+++ src/tests/coap-encoder-test.cpp
@@ -0,0 +1,10 @@
+#include <catch.hpp>
+#include "../coap-encoder.h"
+
+using namespace moducom::coap;
+using namespace moducom::pipeline;
+
+TEST_CASE("CoAP encoder tests", "[coap-encoder]")
+{
+
+}
