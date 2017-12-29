186ee64e5a5a22e8c32e7efb4461fdf7a8da49fb
Malachi Burke <malachi.burke@gmail.com>
2017-12-29 13:43:05 -0800

    Moving experimental prototype encoder from test area to encoder area

--- src/coap-encoder.cpp
+++ src/coap-encoder.cpp
@@ -23,4 +23,84 @@ bool OptionEncoder::process_iterate(pipeline::IBufferedPipelineWriter& writer)
     return state() == CoAP::Parser::OptionValueDone;
 }
 
+
+namespace experimental {
+
+uint8_t ExperimentalPrototypeOptionEncoder1::_option(uint8_t* output_data, number_t number, uint16_t length)
+{
+    moducom::coap::experimental::layer2::OptionBase optionBase(number);
+
+    optionBase.length = length;
+
+    generator.next(optionBase);
+
+    uint8_t pos = 0;
+
+    // OptionValueDone is for non-value version benefit (always reached, whether value is present or not)
+    // OptionValue is for value version benefit, as we manually handle value output
+    CoAP::Parser::SubState isDone = length > 0 ? CoAP::Parser::OptionValue : CoAP::Parser::OptionValueDone;
+
+    while (generator.state() != isDone)
+    {
+        generator_t::output_t output = generator.generate_iterate();
+
+        if (output != generator_t::signal_continue)
+            output_data[pos++] = output;
+    }
+
+    return pos;
+}
+
+
+void ExperimentalPrototypeBlockingOptionEncoder1::option(pipeline::IPipelineWriter& writer, number_t number, uint16_t length)
+{
+    uint8_t buffer[8];
+    // NOTE: Needs underscore on _option as overloading logic can't resolve uint8* vs IPipelineWriter& for some reason
+    uint8_t pos = _option(buffer, number, length);
+
+    writer.write(buffer, pos); // generated header-ish portions of option, sans value
+}
+
+
+void
+ExperimentalPrototypeBlockingOptionEncoder1::option(pipeline::IPipelineWriter& writer, number_t number, const pipeline::MemoryChunk& value)
+{
+    option(writer, number, value.length);
+    writer.write(value); // value portion
+}
+
+
+bool ExperimentalPrototypeNonBlockingOptionEncoder1::option(pipeline::IBufferedPipelineWriter& writer, number_t number,
+                                                            const pipeline::MemoryChunk& value)
+{
+    // Need mini-state machine to determine if we are writing value chunk or
+    // pre-value chunk
+    if (generator.state() == CoAP::Parser::OptionSize)
+    {
+        _option(buffer, number, value.length);
+    }
+
+    // TODO: examine writable to acquire non-blocking count.  Not to be confused with the very similar buffered stuff
+    // writable() doesn't exist yet, but should be a part of the base IPipelineWriter - which is looking more and more
+    // like a stream all the time
+    // NOTE: Probably should draw a distinction between Reader/Writer and PipelineReader/PipelineWriter.  remember,
+    // original purpose of pipeline writes were to carry the memory descriptors themselves around.  It seems I've strayed
+    // from that due to all the pieces floating around here.  Maybe we need it fully broken out:
+    // Reader, BufferedReader, PipelineReader, BufferedPipelineReader
+    //writer.writable();
+}
+
+void ExperimentalPrototypeBlockingPayloadEncoder1::payload(pipeline::IPipelineWriter& writer, const pipeline::MemoryChunk& chunk)
+{
+    if (!marker_written)
+    {
+        writer.write(0xFF);
+        marker_written = true;
+    }
+
+    writer.write(chunk);
+}
+
+}
+
 }}
--- src/coap-encoder.h
+++ src/coap-encoder.h
@@ -7,6 +7,8 @@
 #define MC_COAP_TEST_COAP_ENCODER_H
 
 #include "coap_transmission.h"
+#include "mc/pipeline-writer.h"
+#include "coap-token.h"
 
 namespace moducom { namespace coap {
 
@@ -16,6 +18,200 @@ class OptionEncoder : public experimental::layer2::OptionGenerator::StateMachine
 public:
     bool process_iterate(pipeline::IBufferedPipelineWriter& writer);
 };
+
+namespace experimental {
+
+class ExperimentalSessionContext
+{
+    moducom::coap::layer2::Token token;
+};
+
+typedef CoAP::OptionExperimental::Numbers number_t;
+
+// NOTE: Too dumb to live
+class ExperimentalPrototypeBlockingHeaderEncoder1
+{
+public:
+    void header(pipeline::IPipelineWriter& writer, const CoAP::Header& header)
+    {
+        writer.write(header.bytes, 4);
+    }
+};
+
+
+// NOTE: Too dumb to live
+class ExperimentalPrototypeBlockingTokenEncoder1
+{
+public:
+    void token(pipeline::IPipelineWriter& writer, const pipeline::MemoryChunk& value)
+    {
+        writer.write(value);
+    }
+};
+
+
+class ExperimentalPrototypeOptionEncoder1
+{
+protected:
+    typedef moducom::coap::experimental::layer2::OptionGenerator::StateMachine generator_t;
+    generator_t generator;
+
+    // option helper, fills output with non-value portion of option
+    uint8_t _option(uint8_t* output, number_t number, uint16_t length);
+
+    ExperimentalPrototypeOptionEncoder1() : generator(0) {}
+};
+
+
+class ExperimentalPrototypeNonBlockingOptionEncoder1 : public ExperimentalPrototypeOptionEncoder1
+{
+    uint8_t buffer[8];
+
+protected:
+    bool option(pipeline::IBufferedPipelineWriter& writer, number_t number, const pipeline::MemoryChunk& value);
+};
+
+
+class ExperimentalPrototypeBlockingOptionEncoder1 : public ExperimentalPrototypeOptionEncoder1
+{
+    // option helper
+    void option(pipeline::IPipelineWriter& writer, number_t number, uint16_t length);
+
+public:
+
+    void option(pipeline::IPipelineWriter& writer, number_t number, const pipeline::MemoryChunk& value);
+    void option(pipeline::IPipelineWriter& writer, number_t number)
+    {
+        option(writer, number, 0);
+    }
+};
+
+
+class ExperimentalPrototypeBlockingPayloadEncoder1
+{
+    bool marker_written;
+
+public:
+    ExperimentalPrototypeBlockingPayloadEncoder1() : marker_written(false) {}
+
+    void payload(pipeline::IPipelineWriter& writer, const pipeline::MemoryChunk& chunk);
+};
+
+
+class BlockingEncoder
+{
+private:
+    typedef CoAP::Parser::State state_t;
+    typedef CoAP::Parser _state_t;
+
+#ifdef DEBUG
+    state_t consistency;
+
+    void state(state_t c) { consistency = c; }
+    void assert_state(state_t c)
+    {
+        ASSERT_ERROR(c, consistency, "State mismatch");
+    }
+    void assert_not_state(state_t c)
+    {
+        ASSERT_ERROR(true, c != consistency, "Invalid state");
+    }
+#else
+    void state(state_t) {}
+    void assert_state(state_t) {}
+    void assert_not_state(state_t) {}
+
+#endif
+
+    ExperimentalPrototypeBlockingOptionEncoder1 optionEncoder;
+    ExperimentalPrototypeBlockingPayloadEncoder1 payloadEncoder;
+
+    pipeline::IPipelineWriter& writer;
+
+
+
+public:
+    BlockingEncoder(pipeline::IPipelineWriter& writer) :
+            consistency(_state_t::Header),
+            writer(writer) {}
+
+    void header(const CoAP::Header& header)
+    {
+        assert_state(_state_t::Header);
+        writer.write(header.bytes, 4);
+        state(_state_t::HeaderDone);
+    }
+
+
+    void header(CoAP::Header::RequestMethodEnum request_method, CoAP::Header::TypeEnum c = CoAP::Header::Confirmable)
+    {
+        // initializes with no token and no message id
+        CoAP::Header header(c);
+
+        header.code(request_method);
+
+        this->header(header);
+    }
+
+
+    void header(CoAP::Header::Code::Codes response_method, CoAP::Header::TypeEnum c = CoAP::Header::Confirmable)
+    {
+        CoAP::Header header(c);
+
+        header.response_code(response_method);
+
+        this->header(header);
+    }
+
+
+    // remember that, for now, we have to explicitly set token length in header too
+    void token(const pipeline::MemoryChunk& value)
+    {
+        assert_state(_state_t::HeaderDone);
+        writer.write(value);
+        state(_state_t::TokenDone);
+    }
+
+
+    void token(const moducom::coap::layer2::Token& value)
+    {
+        assert_state(_state_t::HeaderDone);
+        writer.write(value.data(), value.length());
+        state(_state_t::TokenDone);
+    }
+
+    void option(number_t number, const pipeline::MemoryChunk& value)
+    {
+        assert_not_state(_state_t::Header);
+        optionEncoder.option(writer, number, value);
+        state(_state_t::Options);
+    }
+
+    void option(number_t number)
+    {
+        optionEncoder.option(writer, number);
+    }
+
+    void option(number_t number, const char* str)
+    {
+        pipeline::MemoryChunk chunk((uint8_t*) str, strlen(str));
+        option(number, chunk);
+    }
+
+    void payload(const pipeline::MemoryChunk& value)
+    {
+        payloadEncoder.payload(writer, value);
+    }
+
+    void payload(const char* str)
+    {
+        payload(pipeline::MemoryChunk((uint8_t*)str, strlen(str)));
+    }
+};
+
+
+}
+
 
 }}
 
