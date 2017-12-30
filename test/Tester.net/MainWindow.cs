c700dae3edaabad5a5a3f3e246b8eecc4c2774a1
Malachi Burke <malachi.burke@gmail.com>
2017-12-30 12:47:04 -0800

    Adding basic coap requester code, mostly just boilerplate

--- test/Tester.net/MainWindow.cs
+++ test/Tester.net/MainWindow.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using Gtk;
 
+using CoAPNet;
+using CoAPNet.Udp;
+using System.Threading.Tasks;
+using System.Text;
+
 public partial class MainWindow : Gtk.Window
 {
     public MainWindow() : base(Gtk.WindowType.Toplevel)
@@ -12,5 +17,30 @@ public partial class MainWindow : Gtk.Window
     {
         Application.Quit();
         a.RetVal = true;
+    }
+
+    protected void OnButton2Clicked(object sender, EventArgs e)
+    {
+        var client = new CoapClient(new CoapUdpEndPoint());
+
+        var requestor = Task.Run(async () =>
+        {
+            Console.WriteLine("Issuing request");
+
+            CoapMessageIdentifier messageId = await client.GetAsync("coap://127.0.0.1/hello");
+
+            var response = await client.GetResponseAsync(messageId);
+
+            Console.WriteLine("got a response");
+            Console.WriteLine(Encoding.UTF8.GetString(response.Payload));
+        });
+
+        requestor.ContinueWith(x => 
+        {
+            if(x.IsFaulted)
+            {
+                Console.WriteLine($"Got an error: {x.Exception.InnerException.Message}");
+            }
+        });
     }
 }
--- test/Tester.net/gtk-gui/MainWindow.cs
+++ test/Tester.net/gtk-gui/MainWindow.cs
@@ -3,6 +3,10 @@
 
 public partial class MainWindow
 {
+	private global::Gtk.Fixed fixed1;
+
+	private global::Gtk.Button button2;
+
 	protected virtual void Build()
 	{
 		global::Stetic.Gui.Initialize(this);
@@ -10,6 +14,21 @@ public partial class MainWindow
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString("MainWindow");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
+		// Container child MainWindow.Gtk.Container+ContainerChild
+		this.fixed1 = new global::Gtk.Fixed();
+		this.fixed1.Name = "fixed1";
+		this.fixed1.HasWindow = false;
+		// Container child fixed1.Gtk.Fixed+FixedChild
+		this.button2 = new global::Gtk.Button();
+		this.button2.CanFocus = true;
+		this.button2.Name = "button2";
+		this.button2.UseUnderline = true;
+		this.button2.Label = global::Mono.Unix.Catalog.GetString("GtkButton");
+		this.fixed1.Add(this.button2);
+		global::Gtk.Fixed.FixedChild w1 = ((global::Gtk.Fixed.FixedChild)(this.fixed1[this.button2]));
+		w1.X = 33;
+		w1.Y = 25;
+		this.Add(this.fixed1);
 		if ((this.Child != null))
 		{
 			this.Child.ShowAll();
@@ -18,5 +37,6 @@ public partial class MainWindow
 		this.DefaultHeight = 300;
 		this.Show();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler(this.OnDeleteEvent);
+		this.button2.Clicked += new global::System.EventHandler(this.OnButton2Clicked);
 	}
 }
