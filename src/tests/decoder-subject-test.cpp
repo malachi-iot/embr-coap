210f15122e9d26564b2989cfd993919e5a9ecdaf
Malachi Burke <malachi.burke@gmail.com>
2018-03-25 14:55:01 -0700

    Neat that we can pop in IMessageObserver& here

--- src/tests/decoder-subject-test.cpp
+++ src/tests/decoder-subject-test.cpp
@@ -35,7 +35,7 @@ TEST_CASE("CoAP decoder subject tests", "[coap-decoder-subject]")
     SECTION("16 bit detla: Buffer16BitDeltaObserver&")
     {
         Buffer16BitDeltaObserver observer;
-        DecoderSubjectBase<IDispatcherHandler&> test2(observer);
+        DecoderSubjectBase<IMessageObserver&> test2(observer);
         MemoryChunk::readonly_t chunk(buffer_16bit_delta);
         test2.dispatch(chunk);
     }
