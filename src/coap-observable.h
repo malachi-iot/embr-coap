f5598b63c98dd4edbebf52ea4acb9124ce4a05ba
Malachi Burke <malachi.burke@gmail.com>
2018-04-09 17:52:47 -0700

    Beginning foundation for observable/notification code

--- src/coap-observable.h
+++ src/coap-observable.h
@@ -5,4 +5,23 @@
 #ifndef MC_COAP_TEST_COAP_OBSERVABLE_H
 #define MC_COAP_TEST_COAP_OBSERVABLE_H
 
+#include "coap.h"
+#include "coap-token.h"
+
+namespace moducom { namespace  coap {
+
+// Not seeing anything specific in RFC7641 mandating or denying the use of CON or NON
+// messaging for observables, thereby implying we don't need to track what flavor
+// we are in this context either
+struct ObservableContext
+{
+    // increases with each observe notification.  Technically only needs to be 24 bits
+    // so if we do need flags, we can make this into a bit field
+    uint32_t sequence;
+
+    layer1::Token token;
+};
+
+}}
+
 #endif //MC_COAP_TEST_COAP_OBSERVABLE_H
---
+++ src/tests/coap-observable-test.cpp
@@ -0,0 +1,18 @@
+#include <catch.hpp>
+#include "../coap-encoder.h"
+#include "mc/memory.h"
+#include "../coap_transmission.h"
+#include "../coap-token.h"
+#include "../mc/experimental.h"
+#include "../coap-observable.h"
+
+using namespace moducom::coap;
+using namespace moducom::pipeline;
+
+TEST_CASE("CoAP observable (RFC-7641) tests", "[coap-observer]")
+{
+    SECTION("A")
+    {
+
+    }
+}
