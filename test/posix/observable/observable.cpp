cc1ff7798b1f0983167a7c71334932a1eb70952e
Malachi Burke <malachi.burke@gmail.com>
2018-04-10 17:07:14 -0700

    Creating effective boilerplate for observable-test project

---
+++ test/posix/observable/observable.cpp
@@ -0,0 +1,11 @@
+#include <coap.h>
+#include <mc/memory-chunk.h>
+
+using namespace moducom::pipeline;
+
+// TODO: Will need a push-style service_coap_out as well
+
+void service_coap_in(MemoryChunk& inbuf, MemoryChunk& outbuf)
+{
+
+}
