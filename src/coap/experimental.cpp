a787abb858c56756fbdebf860b40acaa0ef66628
Malachi Burke <malachi.burke@gmail.com>
2018-04-12 09:36:41 -0700

    Prepping experimental IP/outgoing buf queue
    Might be redundant with LWIP offering though

--- src/coap/experimental.cpp
+++ src/coap/experimental.cpp
@@ -1,6 +1,22 @@
 #include "experimental-observer.h"
 //#include "estd/queue.h"
+//#include "exp/netbuf.h"
 
 namespace moducom { namespace coap { namespace experimental {
+
+
+// implicitly layer2+layer3
+class OutgoingPacketManager
+{
+    typedef struct { uint8_t raw[4]; } addr_t;
+
+    class Item
+    {
+        addr_t addr;
+        //io::experimental::layer3::NetBufMemoryReader reader;
+        //io::experimental::layer3::NetBufMemoryReader<1> writer;
+    };
+};
+
 
 }}}
