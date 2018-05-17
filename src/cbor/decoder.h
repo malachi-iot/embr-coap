dbe4b368b4993df74d8b6e11c757227fe902b07d
Malachi Burke <malachi.burke@gmail.com>
2018-05-17 05:22:01 -0700

    Misc

--- src/cbor/decoder.h
+++ src/cbor/decoder.h
@@ -6,7 +6,40 @@
 
 namespace moducom { namespace cbor {
 
-class Decoder
+struct Root
+{
+    enum Types
+    {
+        UnsignedInteger = 0,
+        PositiveInteger = 0,
+        NegativeInteger = 1,
+        ByteArray = 2,
+        String = 3,
+        ItemArray = 4,
+        Map = 5,
+        Tag = 6,
+        Float = 7,
+        SimpleData = 7
+    };
+
+
+    // Used during non-simple-data processing
+    enum AdditionalIntegerInformation
+    {
+        // all self contained
+        bits_none = 0,
+
+        bits_8 = 24,
+        bits_16 = 25,
+        bits_32 = 26,
+        bits_64 = 27,
+        Reserved1 = 28,
+        Reserved2 = 30,
+        Indefinite = 31
+    };
+};
+
+class Decoder : public Root
 {
 public:
     bool process_iterate(uint8_t ch);
