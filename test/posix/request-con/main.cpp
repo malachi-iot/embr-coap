fa5c550702c8a1a761c2d73e5f244ea4dd0ec041
Malachi Burke <malachi.burke@gmail.com>
2018-05-21 17:01:43 -0700

    Issued request basically works
    Not yet checking if ACK processing happens exactly right

--- test/posix/request-con/main.cpp
+++ test/posix/request-con/main.cpp
@@ -17,7 +17,8 @@ typedef NetBufDynamicExperimental netbuf_t;
 
 int main()
 {
-    cout << "Requesting" << endl;
+    const char* COAP_SERVER = "192.168.2.3";
+    cout << "Requesting from " << COAP_SERVER << endl;
 
     SocketsDatapumpHelper sdh;
 
@@ -30,9 +31,11 @@ int main()
 
     Header h(Header::Confirmable);
 
+    h.code(Header::Code::Get);
     h.message_id(0x123);
     h.token_length(sizeof(raw_token));
 
+    // TODO: Might be able to check for malformed header at this point (with debug defines = on)
     encoder.header(h);
     encoder.token(raw_token, sizeof(raw_token));
     encoder.option(Option::UriPath, uri);
@@ -41,14 +44,38 @@ int main()
     sockaddr_in addr_out;
 
     addr_out.sin_family = AF_INET;
+    //addr_out.sin_port = COAP_PORT;
     addr_out.sin_port = htons(COAP_PORT);
-    addr_out.sin_addr.s_addr = inet_addr("192.168.2.3");
+    addr_out.sin_addr.s_addr = inet_addr(COAP_SERVER);
 
     sdh.enqueue(std::forward<netbuf_t>(encoder.netbuf()), addr_out);
 
-    //for(;;)
+    for(;;)
     {
         sdh.loop();
+
+        if(!sdh.empty())
+        {
+            sockaddr_in addr_in;
+
+            netbuf_t* response = sdh.front(&addr_in);
+            NetBufDecoder<netbuf_t&> decoder(*response);
+
+            decoder.header();
+            decoder.process_token_experimental();
+
+            option_iterator<netbuf_t> it(decoder);
+
+            while(it.valid())
+            {
+                ++it;
+            }
+
+            // TODO: extract payload
+            //const char* payload = (const char*)decoder.netbuf().unprocessed();
+
+            break;
+        }
     }
 
     return 0;
