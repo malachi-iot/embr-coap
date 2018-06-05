1563bfa8a1121b00df7d07e1b6e314a12b5ea830
Malachi Burke <malachi.burke@gmail.com>
2018-06-05 14:30:03 -0700

    Alas previous commit was a false positive
    In fact, the app-level config_esp is not even referenced now

--- test/pio/esp8266/dtls-datapump/src/mbedtls/config_esp.h
+++ test/pio/esp8266/dtls-datapump/src/mbedtls/config_esp.h
@@ -1,1 +1,22 @@
-config_esp2.h
+// Not getting here, damn
+#error GOT HERE
+
+#include_next<mbedtls/config_esp.h>
+
+#define MBEDTLS_NET_C
+
+#define MBEDTLS_SSL_PROTO_TLS1_2
+#define MBEDTLS_CERTS_C
+#define MBEDTLS_X509_CRT_PARSE_C
+#define MBEDTLS_RSA_C
+#define MBEDTLS_PEM_PARSE_C
+#define MBEDTLS_ENTROPY_C
+
+#define MBEDTLS_SSL_COOKIE_C
+#define MBEDTLS_SSL_CACHE_C
+
+#define MBEDTLS_SSL_SRV_C
+#define MBEDTLS_SSL_PROTO_DTLS
+
+// This combined with SSL_SRV_C enables cookies
+#define MBEDTLS_SSL_DTLS_HELLO_VERIFY
