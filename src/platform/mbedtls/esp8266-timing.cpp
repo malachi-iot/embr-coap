f6a978052595526d0bfec8f47480e2c2f1ab04da
Malachi Burke <malachi.burke@gmail.com>
2018-06-07 00:34:40 -0700

    Fixed mbedtls timers
    bonus: as hoped, it looks like it's FreeRTOS-esque so we should be able
    to use it outside of just esp8266

--- src/platform/mbedtls/esp8266-timing.cpp
+++ src/platform/mbedtls/esp8266-timing.cpp
@@ -15,18 +15,27 @@
 #include "freertos/task.h"
 #endif
 
+//#define DEBUG
+
+// Worked for esp-open-rtos but not for ESP8266_RTOS_SDK
+// (though eventually estdlib might fix that...)
+#ifdef DEBUG_STD
+#include <iostream>
+
+using namespace std;
+#endif
+
 #include <string.h>
 
 static uint32_t get_current_time()
 {
     // lifted from https://bbs.espressif.com/viewtopic.php?t=4835
     // and not tested
-    return xTaskGetTickCount() / portTICK_RATE_MS;
+    return xTaskGetTickCount() * portTICK_RATE_MS;
     // lifted from esp-open-rtos samples
     //return timer_get_count(FRC2) / 5000; // to get roughly 1 ms resolution
+    return pdMS_TO_TICKS(xTaskGetTickCount());
 }
-
-//#define DEBUG
 
 #define TIMING_METHOD_2
 
@@ -45,6 +54,8 @@ extern "C" void mbedtls_timing_set_delay(void* data, uint32_t int_ms, uint32_t f
     ctx->fin_ms = fin_ms == 0 ? 0 : current_time + fin_ms;
 #endif
 #ifdef DEBUG
+    printf("mbedtls_timing_set_delay current seconds: %u (%u)\n", (uint16_t)(current_time / 1000), current_time);
+#elif defined(DEBUG_STD)
     clog << __func__ << " current seconds: " << (uint16_t)(current_time / 1000) << endl;
 #endif
 }
@@ -59,8 +70,10 @@ extern "C" int mbedtls_timing_get_delay(void* data)
 
     auto current_time = get_current_time();
 
-#ifdef DEBUG
+#ifdef DEBUG_STD
     clog << __func__ << " current seconds: " << (uint16_t)(current_time / 1000) << endl;
+#elif defined(DEBUG)
+    printf("mbedtls_timing_get_delay current seconds: %u (%u)\n", (uint16_t)(current_time / 1000), current_time);
 #endif
 
 #ifdef TIMING_METHOD_2
