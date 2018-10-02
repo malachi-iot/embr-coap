5bc0285f7b588c326dbb37f0a04a9a56d5237f10
Malachi Burke <malachi.burke@gmail.com>
2018-10-01 19:28:56 -0700

    Building out discoverable proof of concept posix app

--- test/posix/discoverable/main.cpp
+++ test/posix/discoverable/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 
+#include <platform/posix/sockets-datapump.h>
+
+using namespace moducom::coap;
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "CoRE test" << std::endl;
+
+    SocketsDatapumpHelper sdh;
+
+    for(;;)
+    {
+        sdh.loop(sockets_datapump);
+
+        auto& datapump = sockets_datapump;
+
+        /*
+        typedef TDataPump datapump_t;
+        typedef typename datapump_t::netbuf_t netbuf_t;
+        typedef typename datapump_t::addr_t addr_t;
+        typedef typename datapump_t::Item item_t;
+
+        f(!datapump.dequeue_empty())
+        {
+            item_t& item = datapump.dequeue_front();
+
+            // hold on to ipaddr for when we enqueue a response
+            addr_t ipaddr = item.addr();
+
+            //decoder_t decoder(*sdh.front(&ipaddr, datapump));
+            decoder_t decoder(*item.netbuf());
+
+            layer2::Token token; */
+    }
+
     return 0;
 }
