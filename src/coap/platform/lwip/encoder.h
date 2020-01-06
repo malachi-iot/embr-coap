57111287b7c5c584eac422fb1eeb8e7a0e36eeb5
Malachi Burke <malachi.burke@gmail.com>
2020-01-05 16:40:15 -0800

    Consolidating make_encoder and make_decoder mini-factories

---
+++ src/coap/platform/lwip/encoder.h
@@ -0,0 +1,22 @@
+#include "context.h"
+
+namespace moducom { namespace coap {
+
+
+// in C# we'd do an IEncoderFactory, in C++ for tiny code footprint
+// we'll instead do a special override of make_encoder.  Important since
+// the particulars of how an encoder+streambuf is initialized changes
+// for the underlying streambuf+application in question
+
+// TODO: Best if we can decouple from LwipContext somehow.  Perhaps
+// specialize on a TContext::encoder_type?
+inline LwipContext::encoder_type make_encoder(const LwipContext&)
+{
+    // stock-standard size is 256, which is generally too large
+    // for many CoAP scenarios but still small enough to throw around
+    // in a memory constrained system.  One can (and should) specialize
+    // their context for more specificity
+    return LwipContext::encoder_type(256);
+}
+
+}}
