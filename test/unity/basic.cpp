659dc238f04de08c4d7f36b733ebe83aab09e4de
Malachi Burke <malachi.burke@gmail.com>
2020-01-30 16:13:09 -0800

    Commenting

--- test/unity/basic.cpp
+++ test/unity/basic.cpp
@@ -1,3 +1,5 @@
 // FIX: Reaches out to mc/mem/platform.h - however that's strongly not desired
-// so stalling this until that's surgically removed
+// Additionally, it's broken for Blackfin and the ASSERT code I'm not sure where
+// to put it, so in the short term patch it up for modern use.  Code disabled 
+// until that's done
 //#include <coap/header.h>
