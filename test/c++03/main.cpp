651ff1c443b16ffe2491910fdd52eca95383f1c1
Malachi Burke <malachi.burke@gmail.com>
2020-11-08 12:37:19 -0800

    Bringing online the super-rudimentary existing unity tests

--- test/c++03/main.cpp
+++ test/c++03/main.cpp
@@ -1,6 +1,15 @@
 #include <unit-test.h>
 
+void setUp (void) {}
+void tearDown (void) {}
+
 int main()
 {
+    UNITY_BEGIN();
+    test_decoder();
+    test_encoder();
+    test_header();
+    UNITY_END();
+
     return 0;
 }
