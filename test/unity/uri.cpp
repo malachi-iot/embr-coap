f06418dd2840d96e7b17b50b95f44123b83b1102
Malachi Burke <malachi.burke@gmail.com>
2020-11-09 08:27:15 -0800

    URI path matching passes C++98 tests

--- test/unity/uri.cpp
+++ test/unity/uri.cpp
@@ -6,14 +6,29 @@
 using namespace moducom;
 using namespace uri;
 
-typedef moducom::coap::experimental::UriPathMap map_type;
+typedef coap::experimental::UriPathMap map_type;
 
 void test_uri_match()
 {
     const map_type* result =
             coap::experimental::match(uri::map, 8, "test", id_path_v1_api);
 
-    TEST_ASSERT_EQUAL(id_path_v1_api_test, result->second);
+    TEST_ASSERT_EQUAL_INT(id_path_v1_api_test, result->second);
+}
+
+void test_uri_match2()
+{
+    const map_type* result;
+    coap::experimental::UriPathMatcher3 matcher(uri::map);
+
+    result = matcher.find("v1");
+    TEST_ASSERT_EQUAL_INT(id_path_v1, result->second);
+
+    result = matcher.find("api");
+    TEST_ASSERT_EQUAL_INT(id_path_v1_api, result->second);
+
+    result = matcher.find("power");
+    TEST_ASSERT_EQUAL_INT(id_path_v1_api_power, result->second);
 }
 
 #ifdef ESP_IDF_TESTING
@@ -23,4 +38,5 @@ void test_uri()
 #endif
 {
     RUN_TEST(test_uri_match);
+    RUN_TEST(test_uri_match2);
 }
