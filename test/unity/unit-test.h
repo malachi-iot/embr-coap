ccfa29495d5fc6945c61f4207a1f3ae8d85a6421
Malachi Burke <malachi.burke@gmail.com>
2022-09-11 22:35:34 -0700

    esp-idf Unity tests now online

--- test/unity/unit-test.h
+++ test/unity/unit-test.h
@@ -2,6 +2,10 @@
 
 #include <unity.h>
 
+#if defined(ESP_PLATFORM) && !defined(ARDUINO)
+#define ESP_IDF_TESTING
+#endif
+
 #include "../unit-tests/test-data.h"
 #include "../unit-tests/test-uri-data.h"
 
