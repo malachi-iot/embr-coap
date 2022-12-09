f63aa9b26073c3277d08de5fa538995f43a7b0d8
Malachi Burke <malachi.burke@gmail.com>
2022-12-09 09:53:39 -0800

    Cleanup

--- test/rtos/esp32/setenv.cmake
+++ test/rtos/esp32/setenv.cmake
@@ -2,3 +2,6 @@ set(EXTRA_COMPONENT_DIRS
     "${CMAKE_CURRENT_LIST_DIR}/components")
 
 include($ENV{IDF_PATH}/tools/cmake/project.cmake)
+
+# DEBT: Upgrade estd itself to propagate compiler defines system-wide
+include(${CMAKE_CURRENT_LIST_DIR}/components/estdlib/version_finder.cmake)
