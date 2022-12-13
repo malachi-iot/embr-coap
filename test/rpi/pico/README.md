08077253b5aca224282e454496b3f5480aa2f2e6
Malachi Burke <malachi.burke@gmail.com>
2022-12-13 09:03:43 -0800

    Boilerplate

---
+++ test/rpi/pico/README.md
@@ -0,0 +1,9 @@
+# Raspberry Pi Pico testing
+
+## FreeRTOS
+
+## Results
+
+|   Date  | Project     | Board                | PICO_SDK | FreeRTOS | Result | Notes |
+| ------- | ----------- | -------------------- | -------  | -------- | ------ | ----- |
+| ##DEC22 | echo         |Raspberry Pi Pico W  | v1.4.0   |  none    | ####   | 
---
+++ test/rpi/pico/echo/main.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+
+#include <estd/string.h>
+
+#include <pico/cyw43_arch.h>
+#include <pico/stdio_usb.h>
+
+
+void main_task(__unused void *params)
+{
+    while (true) {
+        static int counter = 0;
+
+        estd::layer1::string<32> s = "Hello, unit test ";
+
+        s += estd::to_string(++counter);
+
+        puts(s.data());
+
+        sleep_ms(5000);
+    }
+}
+
+int main()
+{
+    stdio_init_all();
+
+    for(;;)
+    {
+        // if you are using pico_cyw43_arch_poll, then you must poll periodically from your
+        // main loop (not from a timer) to check for WiFi driver or lwIP work that needs to be done.
+        cyw43_arch_poll();
+        sleep_ms(1);
+    }
+
+    return 0;
+}
