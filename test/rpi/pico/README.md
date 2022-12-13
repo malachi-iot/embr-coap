396efabecb14512e1a7949ef5ac3701de652fc9c
Malachi Burke <malachi.burke@gmail.com>
2022-12-13 10:36:16 -0800

    echo test works

--- test/rpi/pico/README.md
+++ test/rpi/pico/README.md
@@ -6,4 +6,4 @@
 
 |   Date  | Project     | Board                | PICO_SDK | FreeRTOS | Result | Notes |
 | ------- | ----------- | -------------------- | -------  | -------- | ------ | ----- |
-| ##DEC22 | echo         |Raspberry Pi Pico W  | v1.4.0   |  none    | ####   | 
+| 13DEC22 | echo         |Raspberry Pi Pico W  | v1.4.0   |  none    | Pass   | 
--- test/rpi/pico/echo/main.cpp
+++ test/rpi/pico/echo/main.cpp
@@ -2,12 +2,16 @@
 #include <pico/stdlib.h>
 
 #include <estd/string.h>
+#include <estd/ostream.h>
 
 #include <pico/cyw43_arch.h>
 #include <pico/stdio_usb.h>
 
 void udp_coap_init();
 
+using namespace estd;
+
+static pico_ostream clog(stdio_usb);
 
 void main_task(__unused void *params)
 {
@@ -28,7 +32,27 @@ int main()
 {
     stdio_init_all();
 
+    if (cyw43_arch_init())
+    {
+        clog << "failed to initialise" << estd::endl;
+        return 1;
+    }
+
+    cyw43_arch_enable_sta_mode();
+
+    clog << "connecting to " << WIFI_SSID << endl;
+
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000))
+    {
+        clog << "failed to connect: ssid=" << WIFI_SSID;
+        //clog << ", pass=" << WIFI_PASSWORD;
+        clog << endl;
+        return 1;
+    }
+
     udp_coap_init();
+
+    const ip4_addr_t* addr = nullptr;
 
     for(;;)
     {
@@ -36,6 +60,19 @@ int main()
         // main loop (not from a timer) to check for WiFi driver or lwIP work that needs to be done.
         cyw43_arch_poll();
         sleep_ms(1);
+
+        if(addr == nullptr && netif_is_link_up(netif_default))
+        {
+            if(!ip4_addr_isany_val(*netif_ip4_addr(netif_default)))
+            {
+                addr = netif_ip4_addr(netif_default);
+
+                char temp[32];
+                ip4addr_ntoa_r(addr, temp, sizeof(temp));
+
+                clog << "Got IP: " << temp << endl;
+            }
+        }
     }
 
     return 0;
