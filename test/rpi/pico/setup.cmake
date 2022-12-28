0e265b4e9c0b5908f5f258ae46fb89bee2f6ae2f
Malachi Burke <malachi.burke@gmail.com>
2022-12-28 09:51:20 -0800

    Using enhanced layer1::vector + further rpi pico embr::support

--- test/rpi/pico/setup.cmake
+++
@@ -1,5 +0,0 @@
-set(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../../..)
-get_filename_component(ROOT_DIR ${ROOT_DIR} ABSOLUTE)
-
-add_subdirectory(${ROOT_DIR}/ext/embr/test/rpi/pico/support support)
-add_subdirectory(${ROOT_DIR}/src coap)
