04734ee95486ed7e73b388ead50cf8ab3e1974dc
Malachi Burke <malachi.burke@gmail.com>
2023-10-11 09:13:15 -0700

    Phasing out submodule & phasing in FetchContent based approaches

--- tools/cmake/setvars.cmake
+++ tools/cmake/setvars.cmake
@@ -3,3 +3,4 @@ get_filename_component(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUTE)
 set(EMBR_DIR ${ROOT_DIR}/ext/embr)
 set(ESTDLIB_DIR ${EMBR_DIR}/ext/estdlib)
 
+include(${ESTDLIB_DIR}/tools/cmake/fetchcontent.cmake)
