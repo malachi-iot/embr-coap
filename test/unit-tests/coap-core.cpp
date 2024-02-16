00daa50ef1dc1040d22ed9295b0eb37826489020
Malachi Burke <malachi.burke@gmail.com>
2024-02-15 17:26:50 -0800

    Experimentation

--- test/unit-tests/coap-core.cpp
+++ test/unit-tests/coap-core.cpp
@@ -6,6 +6,8 @@
 #include <coap/option.h>
 #include <coap/platform/api.h>  // DEBT: Needed for CoRE... but don't want it to be
 
+#include <estd/sstream.h>
+
 using path = embr::coap::internal::UriPathMap;
 
 enum paths_type
@@ -14,6 +16,8 @@ enum paths_type
     v1_test
 };
 
+// All EXPERIMENTAL at this point
+
 path paths[] =
 {
     { "v1",     v1,         MCCOAP_URIPATH_NONE },
@@ -21,8 +25,7 @@ path paths[] =
     EMBR_COAP_CoRE_PATHS()
 };
 
-template <int, class = void>
-struct coap_core_traits
+struct coap_core_traits_base
 {
     static constexpr const char* n() { return nullptr; }
     static constexpr embr::coap::Option::ContentFormats ct()
@@ -31,8 +34,13 @@ struct coap_core_traits
     }
 };
 
+template <int, class = void>
+struct coap_core_traits : coap_core_traits_base
+{
+};
+
 template <>
-struct coap_core_traits<v1_test>
+struct coap_core_traits<v1_test> : coap_core_traits_base
 {
     static constexpr const char* n() { return "Does Test Things"; }
 };
@@ -45,11 +53,27 @@ void default_core(Streambuf& sb, path paths[], int count)
 
     for(int i = 0; i < count; i++)
     {
-        
+
     }
+}
+
+template <class Streambuf>
+void default_core2(Streambuf& sb, estd::in_place_index_t<0>)
+{
+
+}
+
+template <size_t id, class Streambuf>
+void default_core2(Streambuf& sb, estd::in_place_index_t<id>)
+{
+    if (id == MCCOAP_URIPATH_NONE) return;
+
+    default_core2(sb, estd::in_place_index_t<id - 1>{});
 }
 
 TEST_CASE("CoAP CoRE tests", "[coap-core]")
 {
+    estd::layer1::ostringstream<128> out;
 
+    default_core2<100>(*out.rdbuf(), estd::in_place_index_t<100>{});
 }
