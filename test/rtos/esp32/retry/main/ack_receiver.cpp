6ee979047c2f99c2a35278c99db364d162490cde
Malachi Burke <malachi.burke@gmail.com>
2024-05-17 14:44:27 -0700

    It seems like PBUF_ROM wizardry may be working for us

--- test/rtos/esp32/retry/main/ack_receiver.cpp
+++ test/rtos/esp32/retry/main/ack_receiver.cpp
@@ -100,33 +100,57 @@ static void send_echo_with_con(embr::lwip::udp::Pcb pcb, embr::lwip::Pbuf& pbuf,
     encoder.finalize();
 
     auto& encoder_pbuf = encoder.rdbuf()->pbuf();
+    err_t r;
 
     struct pbuf* p = encoder_pbuf;
+    struct pbuf* p_rom = pbuf_alloc(
+        PBUF_TRANSPORT,
+        //encoder_pbuf.total_length(),
+        0,
+        PBUF_ROM);
+
+    pbuf_chain(p_rom, p);
+
+    r = 0;
+
+    //struct pbuf workaround = *p;
 
     ESP_LOGI(TAG, "about to output %d bytes, ref count=%u",
         encoder_pbuf.total_length(),
         p->ref
         );
 
+    ESP_LOGI(TAG, "chained: tot_len=%u", p_rom->tot_len);
+
     // "pbufs passed to IP must have a ref-count of 1 as their payload pointer
     // gets altered as the packet is passed down the stack"
     //encoder_pbuf.ref();
 
-    pcb.send_experimental(p, endpoint);
+    // Seeing if we can use ROM approach to avoid side-effect of udp_sendto a little
+    // Beware not all platforms support ROM approach https://doc.ecoscentric.com/ref/lwip-basics.html
+
+    r = pcb.send_experimental(p_rom, endpoint);
+    pbuf_free(p_rom);
     auto now = estd::chrono::freertos_clock::now();
     auto t = now.time_since_epoch();
+
+    // https://stackoverflow.com/questions/68786784/is-it-possible-to-use-udp-sendto-to-resend-an-udp-packet
+    // https://lwip-users.nongnu.narkive.com/bexfjxTs/udp-send-space-for-ip-header-is-not-being-allocated
+
+    //*p = workaround;
+    //r = pbuf_header(p, -UDP_HLEN);
     
     // pbuf does not get deallocated automatically on send, according to these guys
     // https://stackoverflow.com/questions/71763904/lwip-when-to-deallocate-the-pbuf-after-calling-udp-sendto
     // https://lwip.fandom.com/wiki/Raw/UDP
 
-    ESP_LOGI(TAG, "exit: phase 2 ref count=%u, p=%p", p->ref, p);
+    ESP_LOGI(TAG, "exit: phase 2 ref count=%u, p=%p, tot_len=%u", p->ref, p, p->tot_len);
 
     //encoder_pbuf.ref();
     tracker.track(t, endpoint, std::move(encoder_pbuf));
     //pbuf_ref(p);
 
-    ESP_LOGI(TAG, "exit: phase 3 ref count=%u", p->ref);
+    ESP_LOGI(TAG, "exit: phase 3 ref count=%u, tot_len=%u", p->ref, p->tot_len);
 }
 
 void udp_coap_recv(void *arg, 
--- test/rtos/esp32/retry/main/app.cpp
+++ test/rtos/esp32/retry/main/app.cpp
@@ -43,24 +43,43 @@ void app_loop()
                 tracker.mark_ack_processed();
             else
             {
-                struct pbuf* p = tracked->buffer();
+                struct pbuf* p_ram = tracked->buffer();
+                struct pbuf* p = pbuf_alloc(
+                    PBUF_TRANSPORT,
+                    0,
+                    PBUF_ROM);
 
-                ESP_LOGI(TAG, "Retransmitting: counter %u, ref count=%u, p=%p, len=%u",
+                pbuf_chain(p, p_ram);
+
+                ESP_LOGI(TAG, "Retransmitting: p_ram ref count=%u, tot_len=%u",
+                    p_ram->ref, p_ram->tot_len);
+
+                ESP_LOGI(TAG, "Retransmitting: counter %u, ref count=%u, p=%p, tot_len=%u",
                     tracked->retransmission_counter,
                     p->ref,
                     p,
-                    p->len
+                    p->tot_len
                     );
 
+                // "ROM" send seems to work better, though there's a glitch
+                // where it fails 2nd send every time.  Feels like it happens
+                // when system is already trying to send from that RAM address.
+                // Maybe though it's due to our glitchy endpoint (0.0.0.0)
+                // DEBT: IIRC ipaddr pointers don't live much past the udp_recv
+                // event
                 err_t r = pcb.send_experimental(
-                    tracked->buffer(),
+                    p,
                     tracked->endpoint()
                     );
 
-                ESP_LOGI(TAG, "Retransmitting: phase 1 %d, ref count=%u",
+                ESP_LOGI(TAG, "Retransmitting: phase 1 r=%d, ref count=%u",
                     r, p->ref);
 
-                tracker.mark_con_sent();
+                pbuf_free(p);
+
+                bool processed = tracker.mark_con_sent();
+
+                ESP_LOGI(TAG, "Retransmitting: processed=%u", processed);
             }
         }
     }
